c6febc365ab27ae6716c954acaf698a899e4086b
Love Poonia <chlovepoonia26@gmail.com>
2025-05-06 20:28:38 +0530

    fix all the error in backend level

--- src/routes/user.js
+++ src/routes/user.js
@@ -35,29 +35,34 @@ userRouter.get("/user/requests/received" , userAuth , async (req,res) =>{
 })
 
 userRouter.get("/user/connections" , userAuth, async (req, res) =>{
-    try {
-        const loginUser = req.user;
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+    .populate("fromUserId", ["firstName", "lastName", "age", "gender", "skills", "about", "photoUrl", "experience" , "linkedinUrl", "githubUrl"]).populate("toUserId" , ["firstName", "lastName", "age", "gender", "skills", "about", "photoUrl", "experience", "linkedinUrl", "githubUrl"]);
+
     
-        const connectionRequest = await ConnectionRequest.find({
-            $or:[
-                {toUserId: loginUser._id, status: "accepted"},
-                {fromUserId: loginUser._id, status: "accepted"},
-            ]
-        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "skills", "about", "photoUrl", "experience" , "linkedinUrl", "githubUrl"]).populate("toUserId" , ["firstName", "lastName", "age", "gender", "skills", "about", "photoUrl", "experience", "linkedinUrl", "githubUrl"]);
+
+    const data = connectionRequests.map((row) => {
     
-        const data = connectionRequest.map((row) => {
-            if(row.fromUserId._id.toString() === loginUser._id.toString()){
-                return row.toUserId;
-            }
-            return row.fromUserId
-        });
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+       
+        return row.toUserId;
+      }
     
-        res.json({
-            data
-        })
-    } catch (error) {
-        res.send(error.message)
-    }
+      return row.fromUserId;
+    });
+    
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
 })
 
 userRouter.get("/user/feed", userAuth, async (req, res) => {
